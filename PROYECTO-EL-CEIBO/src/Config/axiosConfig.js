02852dac796f711639eac8aea1e2aa4e34f51e9a
Michael J. Illescas <143970479+MichaelIllescas@users.noreply.github.com>
2025-02-02 18:11:05 -0300

    Update develop axiosConfig.js

--- PROYECTO-EL-CEIBO/src/Config/axiosConfig.js
+++ PROYECTO-EL-CEIBO/src/Config/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://app.imperial-net.com', // Cambia esto a la URL base de tu API
+  baseURL: 'http://localhost:8080', // Cambia esto a la URL base de tu API
   withCredentials: true, // Permitir el envío de cookies y credenciales
   headers: {
     'Content-Type': 'application/json', // Cambia esto si necesitas otros encabezados
